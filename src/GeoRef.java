8d49360ca226e342c8b6b852436a5e8a18f9b088
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-24 03:11:46 -0400

    Creación Objeto GeoRef

---
+++ src/GeoRef.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Gonzaloafa on 24-06-2014.
+ */
+public class GeoRef {
+
+    private int id;
+    private double x;
+    private double y;
+    private boolean mark;
+
+    public GeoRef(int id, double x, double y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.mark = false;
+    }
+
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public boolean isMark() {
+        return mark;
+    }
+
+    public void setMark(){
+        this.mark = true;
+    }
+
+}
