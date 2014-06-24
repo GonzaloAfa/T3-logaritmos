3352a8c1e0cce67fe88668aa8cdcff2d8b76cae5
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-24 03:16:07 -0400

    Main de prueba

---
+++ src/main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+/**
+ * Created by Gonzaloafa on 24-06-2014.
+ */
+public class main {
+
+    static public void main (String[]args) throws IOException{
+
+        File file = new File("Datos/wi29.txt");
+
+    }
+
+}
