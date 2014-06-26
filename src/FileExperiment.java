8d4edf14b12ea75672aded0f487fd7f64768c91d
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-25 21:53:46 -0400

    Rename Archive

---
+++ src/FileExperiment.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by Gonzaloafa on 24-06-2014.
+ */
+public class FileExperiment {
+
+    private ArrayList<GeoRef> geoRef;
+    private String city = "none";
+    private int size    = 0;
+
+
+    public FileExperiment(String archive){
+
+        DataInputStream in = null;
+
+        try {
+            String strLine;
+            size = 0;
+
+            FileInputStream fileInputStream = new FileInputStream(archive);
+
+            in                  = new DataInputStream(fileInputStream);
+            BufferedReader br   = new BufferedReader(new InputStreamReader(in));
+
+            geoRef = new ArrayList<GeoRef>();
+
+            for (int i = 0; i < 7; i++) {
+                strLine = br.readLine();
+
+                // City
+                if (i == 1) {
+                    city = parseCity(strLine);
+                }
+            }
+
+            while( !(strLine = br.readLine()).equals("EOF") ){
+
+                geoRef.add(parseGeoRef(strLine));
+            }
+
+            in.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // TODO: acá deberia funcionar un in.close(), pero no me resulta xD
+        }
+
+    }
+
+
+    private String parseCity(String data){
+        return data.substring(data.indexOf("in")+1).trim();
+    }
+
+    private int parseDimension(String data){
+        String info = data.substring(data.indexOf(":")+1).trim();
+        return Integer.parseInt(info);
+    }
+
+    private GeoRef parseGeoRef(String data){
+        int p1 = data.indexOf(" ");
+        int p2 = data.indexOf(" ", p1+1);
+
+        int id      = Integer.parseInt(data.substring(0, p1).trim());
+        double x    = Double.parseDouble(data.substring(p1+1, p2).trim());
+        double y    = Double.parseDouble(data.substring(p2+1).trim());
+
+        GeoRef geoRef = new GeoRef(id-1, x, y);
+        return geoRef;
+    }
+
+    public String getCity(){
+        return city;
+    }
+
+    public ArrayList<GeoRef> getGeoRef(){
+        return this.geoRef;
+    }
+
+
+
+
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Gonzaloafa on 24-06-2014.
@@ -9,29 +9,41 @@ public class main {
 
     static public void main (String[]args) throws IOException{
 
-        File file = new File("Datos/zi929.txt");
+        File dir = new File("Datos");
+        String[] ficheros = dir.list();
+
+        // Listado de archivos
+        if (ficheros == null)
+            System.out.println("No hay ficheros en el directorio especificado");
+        else{
+            System.out.println("Ficheros");
+            for (int x=0;x<ficheros.length;x++)
+                System.out.println(ficheros[x]);
+        }
 
 
+
+        FileExperiment file = new FileExperiment("Datos/"+ficheros[0]);
         ArrayList<GeoRef> geoRefs =  file.getGeoRef();
 
-        System.out.println("Experimento Puntos Cercanos");
 
+        System.out.println("Experimento Puntos Cercanos");
         Algorithm experiment = new ClosestPoint();
         experiment.loadData(geoRefs);
         experiment.run();
 
-        System.out.println("Distancia: "+experiment.getRoadDistance());
-        System.out.println("Tiempo: "   +experiment.getTime());
+        System.out.println("Distancia: " + experiment.getRoadDistance());
+        System.out.println("Tiempo: " + experiment.getTime());
+
 
 
         System.out.println("Experimento Heuristica ");
-
         Algorithm experiment2 = new HeuristicConvexHull();
         experiment2.loadData(geoRefs);
         experiment2.run();
 
         System.out.println("Distancia: "+experiment2.getRoadDistance());
-        System.out.println("Tiempo: "   +experiment2.getTime());
+        System.out.println("Tiempo: " + experiment2.getTime());
 
 
     }
