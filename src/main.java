a499dbee12238af6b2a659adce62bf186991b55b
GonzaloAfa <GonzaloAfa@AfaChile.cl>
2014-06-25 20:21:30 -0400

    Se agregan algunas otras pruebas

--- src/main.java
+++ src/main.java
@@ -9,18 +9,30 @@ public class main {
 
     static public void main (String[]args) throws IOException{
 
-        File file = new File("Datos/vm22775.txt");
+        File file = new File("Datos/zi929.txt");
 
 
         ArrayList<GeoRef> geoRefs =  file.getGeoRef();
+
+        System.out.println("Experimento Puntos Cercanos");
 
         Algorithm experiment = new ClosestPoint();
         experiment.loadData(geoRefs);
         experiment.run();
 
+        System.out.println("Distancia: "+experiment.getRoadDistance());
+        System.out.println("Tiempo: "   +experiment.getTime());
+
+
+        System.out.println("Experimento Heuristica ");
+
         Algorithm experiment2 = new HeuristicConvexHull();
         experiment2.loadData(geoRefs);
         experiment2.run();
+
+        System.out.println("Distancia: "+experiment2.getRoadDistance());
+        System.out.println("Tiempo: "   +experiment2.getTime());
+
 
     }
 
